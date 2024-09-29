a538715e64e734198faeb1c4b0c47a46ad63dd44
Momcilo Nikolic <momcilo.pos@gmail.com>
2024-09-30 01:23:46 +0200

    update and reuse component

--- steps/src/App.js
+++ steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   // hanlde previous function
@@ -21,9 +30,9 @@ export default function App() {
 
   return (
     <div>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <buttonnpm className="close" onClick={() => setIsOpen(!isOpen)}>
         &times;
-      </button>
+      </buttonnpm>
       {isOpen && (
         <div className="steps">
           <div className="numbers">
